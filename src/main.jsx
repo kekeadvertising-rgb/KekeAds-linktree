00a78e75566ebcf15c81e0a4f8fc568a15f93c70
kekeadvertising-rgb <kekeadvertising@gmail.com>
2026-05-18 00:15:06 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import KekeAdsLinktree from './KekeAds_Linktree.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <KekeAdsLinktree />
+)
